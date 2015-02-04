7ab47b2854c76cd2e8c40a7ec90ac9555d959721
Imee Cuison <imeetwelve@gmail.com>
2015-02-03 21:01:25 -0500

    not the weather but star wars

--- popup.js
+++ popup.js
@@ -3,21 +3,23 @@
 
 function getWeather () {
   $.ajax({
-        url: 'http://api.openweathermap.org/data/2.5/weather?q=London,uk',
-        type: 'GET',
-        dataType: 'JSONP',
-        success: function (data) {
-          console.log(data);
-          _.each(data, function(weather, idx, arr) {
-            _.each(weather, function(description, idx, arr) {
-            $("body").append('<p>' + description + '</p>');
-          });
-});
-        },
-        error: function (error) {
-          console.log(error);
-        }
-      });
-    }
+   url: 'http://swapi.co/api/people/1/',
+
+   type: 'GET',
+   datatype: 'JSONP',
+   success: function (data) {
+     //console.log(instaPage.memeConfig.baseurl + "_Select_ByTrending";
+     console.log(data);
+     //_.each(data, function(item, idx, arr) {
+      //_.each(item, function (currentItem, index, array) {
+           $('body').append('<p>' + data.name + '</p>');
+//  });
+//});
+ },
+     error: function (error) {
+     console.log(error);
+   }
+ })
+}
 
 getWeather();
